12727b6a76a75e9209854fe7a45a8bc85a87fad5
pingaowang <pingaowang2015@gmail.com>
2023-05-26 18:05:46 -0400

    exp update

--- train_exp_v1_AugRandResizeCrop_1.sh
+++ train_exp_v1_AugRandResizeCrop_1.sh
@@ -1,1 +1,1 @@
-/opt/conda/envs/tsai/bin/python train.py --exp_name stage7_focalloss_lrmax1e-3 --load_model_name stage65_focalloss_lrmax5e-3 --lr_max 1e-3 --bs 8192 --n_epoch 120
+/opt/conda/envs/tsai/bin/python train.py --exp_name AugRandResizeCrop_stage7 --load_model_name stage65_focalloss_lrmax5e-3 --lr_max 1e-3 --bs 8192 --n_epoch 120
