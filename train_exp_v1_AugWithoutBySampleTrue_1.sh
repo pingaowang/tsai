52d056f420a50eb8ff884a04c6e2b04e1ef95897
pingaowang <pingaowang2015@gmail.com>
2023-05-26 20:14:43 -0400

    exp debug

--- train_exp_v1_AugWithoutBySampleTrue_1.sh
+++ train_exp_v1_AugWithoutBySampleTrue_1.sh
@@ -1,1 +1,1 @@
-/opt/conda/envs/tsai/bin/python train.py --exp_name AugWithoutBySampleTrue_stage7 --load_model_name stage65_focalloss_lrmax5e-3 --lr_max 1e-3 --bs 16384 --n_epoch 120
+/opt/conda/envs/tsai/bin/python train.py --exp_name AugWithoutBySampleTrue_stage7 --load_model_name stage65_focalloss_lrmax5e-3 --lr_max 1e-3 --bs 8192 --n_epoch 120
