889dccddeea59c88b67079caf1c015e57c583d37
pingaowang <pingaowang2015@gmail.com>
2023-05-27 00:27:42 -0400

    exp update

--- train.py
+++ train.py
@@ -62,9 +62,9 @@ aug = [
     TSMagScale(p=0.2, magnitude=0.2),
     TSMagWarp(p=0.5, magnitude=0.1),
     TSTimeWarp(p=0.5, magnitude=0.1),
-    # TSMagMulNoise(magnitude=0.1),
-    TSBlur(magnitude=0.1),
-    TSSmooth(magnitude=0.1),
+    TSMagMulNoise(magnitude=0.1),
+    # TSBlur(magnitude=0.1),
+    # TSSmooth(magnitude=0.1),
     # TSInputDropout(magnitude=0.1)
     TSRandomResizedCrop(magnitude=0.2),
     TSRandomCropPad(magnitude=0.2)
