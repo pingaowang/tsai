3d1d4fb62c6a4fc90464a579ffddbc4b205dbe13
pingaowang <pingaowang2015@gmail.com>
2023-05-27 13:45:59 -0400

    exp update

--- train_exp_v1_AugRandAll2_2.sh
+++ train_exp_v1_AugRandAll2_2.sh
@@ -1,1 +1,1 @@
-/opt/conda/envs/tsai/bin/python train.py --exp_name AugRandAll_stage7_2 --load_model_name AugRandAll_stage7 --lr_max 1e-2 --bs 8192 --n_epoch 120
+/opt/conda/envs/tsai/bin/python train.py --exp_name AugRandAll_stage7_2 --load_model_name AugRandAll_stage7 --lr_max 1e-6 --bs 8192 --n_epoch 120
